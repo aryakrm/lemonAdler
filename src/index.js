56d862ea8a617fdb14a9ce948961bdbab4f77382
Arya Karimi <devaryakrm@gmail.com>
2023-01-23 10:53:18 +0300

    INIT

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const AppRoutes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
+
+const routers = createBrowserRouter(AppRoutes);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routers}></RouterProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Hello LemonAdler</h1>
+    </div>
+  );
+}
+
+export default Home;
