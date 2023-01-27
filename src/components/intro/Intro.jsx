a3dcb492226c8b7f592331d9ce2841f7f2f5a0a4
Arya Karimi <devaryakrm@gmail.com>
2023-01-27 13:46:28 +0300

    Intro added and slide deleted

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
+import { GrNext, GrPrevious } from "react-icons/gr";
 
 function Intro() {
   const [index, setIndex] = useState(0);
@@ -9,23 +10,45 @@ function Intro() {
   };
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect} fade pause="false">
-      <Carousel.Item interval={2000}>
-        <img className="d-block w-100" src="/assets/5.jpg" alt="First slide" />
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <img className="d-block w-100" src="/assets/1.jpg" alt="Second slide" />
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <img className="d-block w-100" src="/assets/2.jpg" alt="Third slide" />
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <img className="d-block w-100" src="/assets/3.jpg" alt="Third slide" />
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <img className="d-block w-100" src="/assets/4.jpg" alt="Third slide" />
-      </Carousel.Item>
-    </Carousel>
+    <div>
+      <Carousel activeIndex={index} onSelect={handleSelect} fade pause="false">
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src="/assets/5.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src="/assets/1.jpg"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src="/assets/2.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src="/assets/3.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src="/assets/4.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
 
