f5c743d9937c4d920cbe25c8cbd8dec17a9a28ff
Arya Karimi <devaryakrm@gmail.com>
2023-01-30 13:27:40 +0300

    footer icons added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Footer.scss";
+import { ImInstagram } from "react-icons/im";
+import { ImLinkedin } from "react-icons/im";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { ImBehance2 } from "react-icons/im";
+import { FaTwitterSquare } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -7,10 +12,28 @@ function Footer() {
       <div className="contact">
         <p>info@lemonadler-9.com</p>
       </div>
-      <div className="logo">
-        <img src="https://i.ibb.co/H7DtFmb/1.png" alt="LemonAdler-9 Logo" />
+      <div className="foorter-menu">
+        <div className="logo">
+          <img src="https://i.ibb.co/H7DtFmb/1.png" alt="LemonAdler-9 Logo" />
+        </div>
+        <div className="socialMedia">
+          <a href="https://www.instagram.com/lemonadler9">
+            <ImInstagram />
+          </a>
+          <a href="">
+            <ImLinkedin />
+          </a>
+          <a href="">
+            <FaWhatsappSquare />
+          </a>
+          <a href="">
+            <ImBehance2 />
+          </a>
+          <a href="">
+            <FaTwitterSquare />
+          </a>
+        </div>
       </div>
-      <div className="socialMedia"></div>
     </div>
   );
 }
